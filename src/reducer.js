df1502375e13a6bdd065e772d7c16313e5490f97
Хохлов Андрей Анатольевич <A.Hohlov@transtelematica.ru>
2024-09-05 16:24:42 +0300

    Add file reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,15 +1,24 @@
+import { FIELD_PARAM } from './constants';
+
 const initialState = {
-	// ...
+	currentPlayer: 'X',
+	isGameEnded: false,
+	isDraw: false,
+	field: FIELD_PARAM,
 };
 
 export const reducer = (state = initialState, { type, payload }) => {
 	switch (type) {
 		case 'SET_CURRENT_PLAYER':
 			return {
-				// ...
+				...state,
+				currentPlayer: payload,
 			};
 		case 'SET_FIELD':
-		// ...
+			return {
+				...state,
+				field: payload,
+			};
 		case 'RESTART_GAME':
 			return initialState;
 		default:
